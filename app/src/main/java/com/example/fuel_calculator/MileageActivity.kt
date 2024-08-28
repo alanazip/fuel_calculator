f757f783c7a4eedd92218e24f2b5c2d56c8b843d
Alana de Souza <alananascimentodesouza@gmail.com>
2024-08-28 15:54:09 -0300

    arredondamento

--- app/src/main/java/com/example/fuel_calculator/MileageActivity.kt
+++ app/src/main/java/com/example/fuel_calculator/MileageActivity.kt
@@ -2,13 +2,34 @@ package com.example.fuel_calculator
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button
+import android.widget.EditText
 
-class MileageActivity : AppCompatActivity() {
+class MileageActivity() : AppCompatActivity(), Parcelable {
+    constructor(parcel: Parcel) : this()
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {}
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MileageActivity> {
+        override fun createFromParcel(parcel: Parcel): MileageActivity {
+            return MileageActivity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MileageActivity?> {
+            return arrayOfNulls(size)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,17 +39,17 @@ class MileageActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button: Button = findViewById(R.id.button4)
 
+        // Recebe os valores passados pela FuelPriceActivity
+        val precoCombustivel = intent.getDoubleExtra("precoCombustivel", 0.0)
+        val consumoPorLitro = intent.getDoubleExtra("consumoPorLitro", 0.0)
+        val quilometragem = intent.getDoubleExtra("quilometragem", 0.0)
+
+        // Configura o botão
+        val button: Button = findViewById(R.id.button4)
         button.setOnClickListener {
+            // Cria o intent para passar os dados para a próxima atividade
             val intent = Intent(this, ResultActivity::class.java)
-            startActivity(intent)
-        }
-        button.setOnClickListener {
-            val intent = Intent(this, ResultActivity::class.java)
-            val precoCombustivel = 1.70 // Substitua pelo valor real
-            val consumoPorLitro = 5.0 // Substitua pelo valor real
-            val quilometragem = 150.0 // Substitua pelo valor real
             intent.putExtra("precoCombustivel", precoCombustivel)
             intent.putExtra("consumoPorLitro", consumoPorLitro)
             intent.putExtra("quilometragem", quilometragem)
