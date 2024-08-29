267b49de714b64c8d192a2cf317edfbaf0d768df
Alana de Souza <alananascimentodesouza@gmail.com>
2024-08-29 15:48:47 -0300

    arredondamento

--- app/src/main/java/com/example/fuel_calculator/ResultActivity.kt
+++ app/src/main/java/com/example/fuel_calculator/ResultActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.fuel_calculator.databinding.ActivityResultBinding
+
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -15,30 +16,27 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        // Localize o botão no layout
-        val button: Button = findViewById(R.id.button6)
-        button.setOnClickListener {
-            val precoCombustivel = 1.70 // Substitua pelo valor real
-            val consumoPorLitro = 5.0 // Substitua pelo valor real
-            val quilometragem = 150.0 // Substitua pelo valor real
-            val intent = Intent(this, ResultActivity::class.java)
-            intent.putExtra("precoCombustivel", precoCombustivel)
-            intent.putExtra("consumoPorLitro", consumoPorLitro)
-            intent.putExtra("quilometragem", quilometragem)
-            startActivity(intent)
-        }
 
-        // Configura o clique do botão para navegar para MainActivity
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-
-        }
+        // Recebe os valores passados da tela anterior
         val precoCombustivel = intent.getDoubleExtra("precoCombustivel", 0.0)
         val consumoPorLitro = intent.getDoubleExtra("consumoPorLitro", 0.0)
         val quilometragem = intent.getDoubleExtra("quilometragem", 0.0)
+
+        // Calcula o consumo e o custo total
         val consumo = quilometragem / consumoPorLitro
         val custoTotal = consumo * precoCombustivel
+
+        // Exibe os valores na tela
+        findViewById<TextView>(R.id.textView10).text = "Preço do Combustível: $precoCombustivel"
+        findViewById<TextView>(R.id.textView14).text = "Consumo por Litro: $consumoPorLitro"
+        findViewById<TextView>(R.id.textView15).text = "Quilometragem: $quilometragem"
         findViewById<TextView>(R.id.textView5).text = "Custo total: $custoTotal"
+
+        // Configura o botão para retornar à MainActivity
+        val button: Button = findViewById(R.id.button6)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
