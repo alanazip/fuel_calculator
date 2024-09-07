444a7b5cda16d687145c5623216a93242d1c448e
Alana de Souza <alananascimentodesouza@gmail.com>
2024-09-07 01:47:18 -0300

    finalproject

--- app/src/main/java/com/example/fuel_calculator/ResultActivity.kt
+++ app/src/main/java/com/example/fuel_calculator/ResultActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.fuel_calculator.databinding.ActivityResultBinding
+import java.text.DecimalFormat
 
 class ResultActivity : AppCompatActivity() {
 
@@ -22,17 +23,20 @@ class ResultActivity : AppCompatActivity() {
         val consumoPorLitro = intent.getDoubleExtra("consumoPorLitro", 0.0)
         val quilometragem = intent.getDoubleExtra("quilometragem", 0.0)
 
-        // Calcula o consumo e o custo total
         val consumo = quilometragem / consumoPorLitro
         val custoTotal = consumo * precoCombustivel
 
-        // Exibe os valores na tela
-        findViewById<TextView>(R.id.textView10).text = "Preço do Combustível: $precoCombustivel"
-        findViewById<TextView>(R.id.textView14).text = "Consumo por Litro: $consumoPorLitro"
-        findViewById<TextView>(R.id.textView15).text = "Quilometragem: $quilometragem"
-        findViewById<TextView>(R.id.textView5).text = "Custo total: $custoTotal"
+        val decimalFormat = DecimalFormat("#.##")
+        val precoCombustivelFormatado = decimalFormat.format(precoCombustivel)
+        val consumoPorLitroFormatado = decimalFormat.format(consumoPorLitro)
+        val quilometragemFormatada = decimalFormat.format(quilometragem)
+        val custoTotalFormatado = decimalFormat.format(custoTotal)
 
-        // Configura o botão para retornar à MainActivity
+        findViewById<TextView>(R.id.textView10).text = "Preço do Combustível: R$$precoCombustivelFormatado"
+        findViewById<TextView>(R.id.textView14).text = "Consumo por Litro: $consumoPorLitroFormatado L"
+        findViewById<TextView>(R.id.textView15).text = "Quilometragem: $quilometragemFormatada Km"
+        findViewById<TextView>(R.id.textView5).text = "Custo total: R$$custoTotalFormatado"
+
         val button: Button = findViewById(R.id.button6)
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
